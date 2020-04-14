250f9d00715965f29a32cc3ec8fd4df57026e6a3
Erik Fredericks <fredericks@oakland.edu>
2020-04-14 14:30:21 +0100

    Updated main to send data to Unity

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
 import socket
+import time
+import json
 from sense_hat import SenseHat
 
 # Initialize sense hat
 sh = SenseHat()
 sh.set_imu_config(False, True, False)
 
+IP_ADDR = "192.168.1.18"
+IP_PORT = 50001
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+while True:
+  try:
+    gyro = sh.get_gyroscope()
+    #txData = '{"pitch":' + str(gyro['pitch']) + ',"roll":' + str(gyro['roll']) + ',"yaw":' + str(gyro['yaw']) + '}'
+    
+    txData = "{pitch};{roll};{yaw}".format(**gyro)
+    print("Sending [{0}] to [{1}:{2}]".format(txData, IP_ADDR, IP_PORT))
+    sock.sendto(bytes(txData, "utf-8"), (IP_ADDR, IP_PORT))
+    #time.sleep(1)
+
+  except Exception as e:
+    print("Error: {0}".format(e))
+    sock.close()
+    break
 
 
+"""
 backlog = 1
 size = 1024
 
@@ -15,6 +36,22 @@ s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind(('0.0.0.0', 50001))
 s.listen(backlog)
 
+while True:
+  try:
+    client, address = s.accept()
+    bytes_received = client.recv(size)
+
+    gyro = sh.get_gyroscope()
+    txData = "{pitch};{roll};{yaw}".format(**gyro)
+    client.send(txData.encode())
+    time.sleep(1)
+  except Exception as e:
+    print("Error: {0}".format(e))
+    client.close()
+    break
+
+"""
+"""
 try:
   print("I'm waiting...")
   client, address = s.accept()
@@ -25,7 +62,10 @@ try:
       print(str(data))
 
     gyro = sh.get_gyroscope()
-    print("p: {pitch}, r: {roll}, y: {yaw}".format(**gyro_only))
+    txData = "{pitch};{roll};{yaw}".format(**gyro)
+    print(txData)
+    client.sendall(txData)#txData);
+    #print("p: {pitch}, r: {roll}, y: {yaw}".format(**gyro))
  
     
 
@@ -33,3 +73,4 @@ except:
   print("Closing socket")
   client.close()
   s.close()
+"""
